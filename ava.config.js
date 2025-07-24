17348a48f5254312a9cc584c37566d67b06f66ae
Raymond Z <raykyri@gmail.com>
2025-07-24 18:17:12 -0400

    fix: update tests

--- ava.config.js
+++ ava.config.js
@@ -1,10 +1,4 @@
 export default {
-	files: ["./test/*.test.ts"],
+	files: ["./lib/test/*.test.js"],
 	concurrency: 1,
-	typescript: {
-		compile: false,
-		rewritePaths: {
-			"test/": "test/lib/",
-		},
-	},
 }
